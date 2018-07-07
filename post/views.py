a65b622f66c6622705cb8d61161522709455890f
Your Name <one.@163.com>
2018-07-07 11:41:53 +0800

    初步页面完成 ！

--- post/views.py
+++ post/views.py
@@ -1,30 +1,54 @@
+from math import ceil
+
 from django.shortcuts import render, redirect
 
-
-# Create your views here.
+from post.models import Post
 
 
 def create_post(request):
-    if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        post = request.POST.get("title=title, content=content")
-        return redirect("/post/read/?post_id=%s" % post.id)
-    return render(request, "create_post.html", {})
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%s' % post.id)
+    return render(request, 'create_post.html')
 
 
 def edit_post(request):
-    return render(request, "edit_post.html", {})
+
+    if request.method == 'POST':
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(id=post_id)
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%s' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(id=post_id)
+        return render(request, 'edit_post.html', {'post': post})
 
 
 def read_post(request):
-    return render(request, "read_post.html", {})
+    post_id = int(request.GET.get('post_id'))
+    post = Post.objects.get(id=post_id)
+    return render(request, 'read_post.html', {'post': post})
 
 
 def post_list(request):
-    return render(request, "post_list.html", {})
+    page = int(request.GET.get('page', 1))  # 页码
 
+    per_page = 10                   # 每页的文章数
+    total = Post.objects.count()    # 帖子总数
+    pages = ceil(total / per_page)  # 总页数
+
+    start = (page - 1) * per_page
+    end = start + per_page
+    posts = Post.objects.all()[start:end]
+    return render(request, 'post_list.html', {'posts': posts, 'pages': range(pages)})
 
 
 def search(request):
-    return render(request, "search.html", {})
+    keyword = request.POST.get('keyword')
+    posts = Post.objects.filter(content__contains=keyword)
+    return render(request, 'search.html', {'posts': posts})
